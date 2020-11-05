983b47cd24d6d1d55b66c897b9e11c2666403a54
unknown <stdioh321@gmail.com>
2020-11-05 08:13:51 -0300

    Minor]

--- ygo/lib/components/card_viewer.dart
+++ ygo/lib/components/card_viewer.dart
@@ -16,38 +16,91 @@ class CardViewer extends StatefulWidget {
 
 class _CardViewerState extends State<CardViewer> {
   PageController _pageCtrl = PageController();
+  PhotoViewController _photoCtrl;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _photoCtrl = PhotoViewController();
+
+    // _pageCtrl.addListener(() {
+    //   print('_pageCtrl.addListener');
+
+    // });
+  }
+
+  double currPage() {
+    double _page = 0;
+    try {
+      _page = _pageCtrl.page;
+    } catch (e) {
+      print('currPage ERROR');
+    }
+    return _page;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.card.name, style: TextStyle(fontSize: 13)),
-      ),
-      body: GestureDetector(
-        onTap: () {
-          print(_pageCtrl.page);
-        },
-        child: Container(
-            // child: PhotoView(
-            //   enableRotation: true,
-            //   imageProvider: NetworkImage(card.cardImages[0].imageUrl),
-            // ),
-            child: PhotoViewGallery.builder(
-                pageController: _pageCtrl,
-                itemCount: widget.card.cardImages.length ?? 0,
-                loadingBuilder: (context, event) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                },
-                builder: (_, idx) {
-                  return PhotoViewGalleryPageOptions(
-                    imageProvider: NetworkImage(
-                      widget.card.cardImages[idx].imageUrl,
-                    ),
-                  );
-                })),
-      ),
+    return FutureBuilder(
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        return Scaffold(
+          floatingActionButton: _pageCtrl?.page < widget.card.cardImages.length
+              ? FloatingActionButton(
+                  onPressed: () {
+                    try {
+                      // print(_pageCtrl.page ?? "Nothing");
+                      _pageCtrl.nextPage(
+                        curve: Curves.easeInCubic,
+                        duration: Duration(milliseconds: 100),
+                      );
+                    } catch (e) {}
+                  },
+                  child: Icon(Icons.arrow_forward),
+                )
+              : SizedBox(),
+          appBar: AppBar(
+            title: Text(widget.card.name, style: TextStyle(fontSize: 13)),
+          ),
+          body: GestureDetector(
+            onTap: () {
+              print(_pageCtrl.page);
+            },
+            child: Container(
+                // child: PhotoView(
+                //   enableRotation: true,
+                //   imageProvider: NetworkImage(card.cardImages[0].imageUrl),
+                // ),
+                child: Stack(
+              children: [
+                PhotoViewGallery.builder(
+                  pageController: _pageCtrl,
+                  // onPageChanged: ,
+                  itemCount: widget.card.cardImages.length ?? 0,
+                  loadingBuilder: (context, event) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  },
+                  builder: (_, idx) {
+                    return PhotoViewGalleryPageOptions(
+                      controller: _photoCtrl,
+                      tightMode: true,
+                      imageProvider: NetworkImage(
+                        widget.card.cardImages[idx].imageUrl,
+                      ),
+                    );
+                  },
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: Text("HERE: ${currPage()}"),
+                ),
+              ],
+            )),
+          ),
+        );
+      },
     );
   }
 }
